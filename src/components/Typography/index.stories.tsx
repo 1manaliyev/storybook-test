ba629ee078a50e546d68dd2628a7ec2cdd0fc05e
1manaliyev <baxtik.kkp@mail.ru>
2025-03-21 01:44:56 +0500

    fixed typography

--- src/components/Typography/index.stories.tsx
+++ src/components/Typography/index.stories.tsx
@@ -11,199 +11,89 @@ export default meta;
 
 type Story = StoryObj<typeof Typography>;
 
-export const H1: Story = {
+export const Title1: Story = {
   args: {
     children: "Text example",
-    variant: "h1Title",
+    variant: "t1",
   },
 };
 
-export const H2: Story = {
+export const Title2: Story = {
   args: {
     children: "Text example",
-    variant: "h2Title",
+    variant: "t1",
   },
 };
 
-export const H3: Story = {
+export const Title3: Story = {
   args: {
     children: "Text example",
-    variant: "h3Title",
+    variant: "t3",
   },
 };
 
-export const H4: Story = {
+export const Title4: Story = {
   args: {
     children: "Text example",
-    variant: "h4Title",
+    variant: "t4",
   },
 };
 
-export const H5: Story = {
+export const Headline1: Story = {
   args: {
     children: "Text example",
-    variant: "h5Title",
+    variant: "h1",
   },
 };
 
-export const H6: Story = {
+export const Headline2: Story = {
   args: {
     children: "Text example",
-    variant: "h6Title",
+    variant: "h2",
   },
 };
 
-export const H7: Story = {
+export const Accent1: Story = {
   args: {
     children: "Text example",
-    variant: "h7Title",
+    variant: "a1",
   },
 };
 
-export const H8: Story = {
-  args: {
-    children: "Text example",
-    variant: "h8Title",
-  },
-};
-
-export const H9: Story = {
-  args: {
-    children: "Text example",
-    variant: "h9Title",
-  },
-};
-
-export const H10: Story = {
-  args: {
-    children: "Text example",
-    variant: "h10Title",
-  },
-};
-
-export const H10_1: Story = {
-  args: {
-    children: "Text example",
-    variant: "h10_1Title",
-  },
-};
-
-export const H11: Story = {
-  args: {
-    children: "Text example",
-    variant: "h11Title",
-  },
-};
-
-export const H12: Story = {
-  args: {
-    children: "Text example",
-    variant: "h12Title",
-  },
-};
-
-export const B1: Story = {
+export const BodyText: Story = {
   args: {
     children: "Text example",
     variant: "b1",
   },
 };
 
-export const B1_1: Story = {
-  args: {
-    children: "Text example",
-    variant: "b1_1",
-  },
-};
-
-export const B2: Story = {
+export const CaptionCaps: Story = {
   args: {
     children: "Text example",
     variant: "b2",
   },
 };
 
-export const B3: Story = {
+export const FootnoteParagraph: Story = {
   args: {
     children: "Text example",
-    variant: "b3",
-  },
-};
-
-export const B4: Story = {
-  args: {
-    children: "Text example",
-    variant: "b4",
-  },
-};
-
-export const B5: Story = {
-  args: {
-    children: "Text example",
-    variant: "b5",
-  },
-};
-
-export const B6: Story = {
-  args: {
-    children: "Text example",
-    variant: "b6",
-  },
-};
-
-export const B7: Story = {
-  args: {
-    children: "Text example",
-    variant: "b7",
-  },
-};
-
-export const B8: Story = {
-  args: {
-    children: "Text example",
-    variant: "b8",
+    variant: "f1",
   },
 };
 
 export const AllTypography: Story = {
   render: () => (
     <div style={{ display: "flex", flexDirection: "column", gap: "30px" }}>
-      <Typography variant="h1Title">H1 Heading 1</Typography>
-      <Typography variant="h2Title">H2 Heading for mobile</Typography>
-      <Typography variant="h3Title">H3 Heading 1</Typography>
-      <Typography variant="h4Title">H4 NFT name on collections page</Typography>
-      <Typography variant="h5Title">H5 Heading 1</Typography>
-      <Typography variant="h6Title">H6 NFT Collections Performance</Typography>
-      <Typography variant="h7Title">
-        H7 Display of purchased tokens + time until the end of the auction
-      </Typography>
-      <Typography variant="h8Title">H8 Heading 1</Typography>
-      <Typography variant="h9Title">H9 Heading 1</Typography>
-      <Typography variant="h10Title">
-        H10 Text in header, on buttons, in NFT collections, accordion, in chips
-        (Choice chips)
-      </Typography>
-      <Typography variant="h10_1Title">H10.1 Heading 1</Typography>
-      <Typography variant="h11Title">H11 Footer Heading</Typography>
-      <Typography variant="h12Title">H12 Heading 1</Typography>
-      <Typography variant="b1">
-        B1 Main text, balance display, sidebar
-      </Typography>
-      <Typography variant="b1_1">B1.1 Heading 1</Typography>
-      <Typography variant="b2">B2 Heading 1</Typography>
-      <Typography variant="b3">
-        B3 Names of NFT collections, text on the additional information button
-      </Typography>
-      <Typography variant="b4">B4 Heading 1</Typography>
-      <Typography variant="b5">B5 Heading 1</Typography>
-      <Typography variant="b6">B6 Heading 1</Typography>
-      <Typography variant="b7">
-        B7 Search button text, subtitle in NFT collections, main text footer
-      </Typography>
-      <Typography variant="b8">B8 Heading 1</Typography>
-      <Typography variant="b9">
-        B9 Explanatory text for NFT collection indicators
-      </Typography>
+      <Typography variant="t1">T1 Title 1</Typography>
+      <Typography variant="t2">T2 Title 2</Typography>
+      <Typography variant="t3">T3 Title 3</Typography>
+      <Typography variant="t4">T4 Title 4</Typography>
+      <Typography variant="h1">H1 Headline 1</Typography>
+      <Typography variant="h2">H2 Headline 2</Typography>
+      <Typography variant="a1">A1 Accent 1</Typography>
+      <Typography variant="b1">B1 Body text</Typography>
+      <Typography variant="b2">B2 Caption Caps</Typography>
+      <Typography variant="f1">F1 Footnote paragraph </Typography>
     </div>
   ),
 };
--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -5,80 +5,41 @@ import s from "./Typography.module.scss";
 interface Props {
   children: JSX.Element | string;
   variant?:
-    | "h1Title"
-    | "h2Title"
-    | "h3Title"
-    | "h4Title"
-    | "h5Title"
-    | "h6Title"
-    | "h7Title"
-    | "h8Title"
-    | "h9Title"
-    | "h10Title"
-    | "h10_1Title"
-    | "h11Title"
-    | "h12Title"
+    | "t1"
+    | "t2"
+    | "t3"
+    | "t4"
+    | "h1"
+    | "h2"
+    | "a1"
     | "b1"
-    | "b1_1"
     | "b2"
-    | "b3"
-    | "b4"
-    | "b5"
-    | "b6"
-    | "b7"
-    | "b8"
-    | "b9"
+    | "f1";
 }
 
 const Typography: FC<Props> = ({ children, variant = "b1" }) => {
   const className = useMemo(() => {
     switch (variant) {
-      case "h1Title":
+      case "t1":
+        return s.t1;
+      case "t2":
+        return s.t2;
+      case "t3":
+        return s.t3;
+      case "t4":
+        return s.t4;
+      case "h1":
         return s.h1;
-      case "h2Title":
+      case "h2":
         return s.h2;
-      case "h3Title":
-        return s.h3;
-      case "h4Title":
-        return s.h4;
-      case "h5Title":
-        return s.h5;
-      case "h6Title":
-        return s.h6;
-      case "h7Title":
-        return s.h7;
-      case "h8Title":
-        return s.h8;
-      case "h9Title":
-        return s.h9;
-      case "h10Title":
-        return s.h10;
-      case "h10_1Title":
-        return s.h10_1;
-      case "h11Title":
-        return s.h11;
-      case "h12Title":
-        return s.h12;
+      case "a1":
+        return s.a1;
       case "b1":
         return s.b1;
-      case "b1_1":
-        return s.b1_1;
       case "b2":
         return s.b2;
-      case "b3":
-        return s.b3;
-      case "b4":
-        return s.b4;
-      case "b5":
-        return s.b5;
-      case "b6":
-        return s.b6;
-      case "b7":
-        return s.b7;
-      case "b8":
-        return s.b8;
-      case "b9":
-        return s.b9;
+      case "f1":
+        return s.f1;
     }
   }, []);
 
